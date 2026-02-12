3a707e599e8ee00ce5f8c2ea0da448d5876fac14
kkartikey75way-blip <kkartikey.75way@gmail.com>
2026-02-12 18:40:39 +0530

    chore: stop tracking backend dist folder

--- digital-asset-backend/dist/routes/analytics.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const analytics_controller_1 = require("../controllers/analytics.controller");
-const auth_middleware_1 = require("../middleware/auth.middleware");
-const router = (0, express_1.Router)();
-router.get("/", auth_middleware_1.protect, analytics_controller_1.analyticsController);
-exports.default = router;
